f5b0d29b67bbedd7e76c2ba9b314a7b7fb1318a3
unknown <lars.teeffelen@gmail.com>
2020-12-09 22:42:16 +0100

    More pages and responsiveness
    Added all the pages with a "No content" post rather than no page.
    
    More tweaks done to the compatibility for older, smaller devices.

---
+++ scripts/navbar.js
@@ -0,0 +1,21 @@
+/*
+Auteur: Lars van Teeffelen
+Aanmaakdatum: 11-25-2020
+
+Omschrijving: JavaScript bestand test
+*/
+
+
+
+function openBurgerMenu() {
+    if(document.getElementById("regularMenu").style.display == "none") 
+    {
+        document.getElementById("regularMenu").style.display = "block";
+    } 
+    else 
+    {
+        document.getElementById("regularMenu").style.display = "none";
+    }
+}
+
+
