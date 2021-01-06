f93bc5a5bdd753581325823cdac37838e9f9b0ae
unknown <lars.teeffelen@gmail.com>
2021-01-06 22:30:46 +0100

    Updates
    New animations. Fixes of double clicking the menu and more responsiveness and information

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,13 +6,21 @@ Omschrijving: JavaScript bestand test
 */
 
 function openBurgerMenu() {
-    if(document.getElementById("regularMenu").style.display == "none") 
+    if(document.getElementById("regularMenu").style.display == "none" || document.getElementById("regularMenu").style.display == "") 
     {
         document.getElementById("regularMenu").style.display = "block";
+
+        document.getElementById("line1").style.cssText = "opacity: 1; transform: translateY(12px) rotateZ(45deg); transition: 0.5s;";
+        document.getElementById("line2").style.cssText = "opacity: 0; transition: 0.5s;"
+        document.getElementById("line3").style.cssText = "opacity: 1; transform: translateY(-8px) rotate(-45deg); transition: 0.5s;";
     } 
     else 
     {
         document.getElementById("regularMenu").style.display = "none";
+        
+        document.getElementById("line1").style.cssText = "opacity: 1; transform: translateY(0px); transition: 0.5s;";
+        document.getElementById("line2").style.cssText = "opacity: 1; transition: 0.5s;"
+        document.getElementById("line3").style.cssText = "opacity: 1; transform: translateY(0px); transition: 0.5s;";
     }
 }
 
